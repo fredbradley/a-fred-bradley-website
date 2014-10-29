7504ab9870cd79a1bd1646eab7d7798ffde6a7e7
fredbradley <hello@fredbradley.co.uk>
2014-10-29 10:03:05 -0700

    Full Plugin

---
+++ classes/class.fred.options.php
@@ -0,0 +1,328 @@
+<?php
+require_once dirname( __FILE__ ) . '/class.settingsapiwrapper.php';
+
+/**
+ * WordPress settings API demo class
+ * @url https://github.com/tareq1988/wordpress-settings-api-class
+ * @author Tareq Hasan
+ */
+if ( !class_exists('WeDevs_Settings_API_Test' ) ):
+class WeDevs_Settings_API_Test {
+
+    private $settings_api; 
+
+    function __construct() {
+        $this->settings_api = new WeDevs_Settings_API;
+
+        add_action( 'admin_init', array($this, 'admin_init') );
+        add_action( 'admin_menu', array($this, 'admin_menu') );
+    }
+
+    function admin_init() {
+
+        //set the settings
+        $this->settings_api->set_sections( $this->get_settings_sections() );
+        $this->settings_api->set_fields( $this->get_settings_fields() );
+
+        //initialize settings
+        $this->settings_api->admin_init();
+    }
+
+    function admin_menu() {
+        add_menu_page( 'Website Options', 'Website Options', 'edit_theme_options', 'jack_options_page', array($this, 'plugin_page'), plugins_url('/images/websiteby.png', dirname(__FILE__)) );
+    }
+  
+    
+
+    function get_settings_sections() {
+        $sections = array(
+        	array(
+        		'id' => 'site_global_options',
+        		'title' => __("Site Options", "jack"),
+        	),
+        	        	
+            /*array(
+                'id' => 'wedevs_basics',
+                'title' => __( 'Basic Settings', 'wedevs' )
+            ),
+            array(
+                'id' => 'wedevs_advanced',
+                'title' => __( 'Advanced Settings', 'wedevs' )
+            ),
+            array(
+                'id' => 'wedevs_others',
+                'title' => __( 'Other Settings', 'wpuf' )
+            )*/
+        );
+        return $sections;
+    }
+	function do_social_fields($input) {
+		$name = "jack_".strtolower($input);
+		$label = $input." URL";
+		$type = 'text';
+		
+		return array(
+			'name' => $name,
+			'label' => $label,
+			'type' => $type
+		);
+	}
+    /**
+     * Returns all the settings fields
+     *
+     * @return array settings fields
+     */
+    function get_settings_fields() {
+        $settings_fields = array(
+        	'site_global_options' => array(
+        		array(
+                    'name' => 'site_logo',
+                    'label' => __( 'Site Logo', 'wedevs' ),
+                    'desc' => __( '', 'wedevs' ),
+                    'type' => 'file',
+                    'default' => ''
+                ),
+                array(
+                	'name' => 'interview_photos', 
+                	'label' => __('Interview Photo IDs', 'fred'),
+                	'desc' => "Find the ID of the photo and make a comment separated list here",
+                	'type' => 'text'
+                ),
+                $this->do_social_fields("Facebook"),
+                $this->do_social_fields("Twitter"),
+                $this->do_social_fields("Instagram"),
+                $this->do_social_fields("Soundcloud"),
+                $this->do_social_fields("LinkedIn"),
+                $this->do_social_fields("GooglePlus")
+        	),
+          /*  'wedevs_basics' => array(
+                array(
+                    'name' => 'text_val',
+                    'label' => __( 'Text Input (integer validation)', 'wedevs' ),
+                    'desc' => __( 'Text input description', 'wedevs' ),
+                    'type' => 'text',
+                    'default' => 'Title',
+                    'sanitize_callback' => 'intval'
+                ),
+                array(
+                    'name' => 'textarea',
+                    'label' => __( 'Textarea Input', 'wedevs' ),
+                    'desc' => __( 'Textarea description', 'wedevs' ),
+                    'type' => 'textarea'
+                ),
+                array(
+                    'name' => 'checkbox',
+                    'label' => __( 'Checkbox', 'wedevs' ),
+                    'desc' => __( 'Checkbox Label', 'wedevs' ),
+                    'type' => 'checkbox'
+                ),
+                array(
+                    'name' => 'radio',
+                    'label' => __( 'Radio Button', 'wedevs' ),
+                    'desc' => __( 'A radio button', 'wedevs' ),
+                    'type' => 'radio',
+                    'options' => array(
+                        'yes' => 'Yes',
+                        'no' => 'No'
+                    )
+                ),
+                array(
+                    'name' => 'multicheck',
+                    'label' => __( 'Multile checkbox', 'wedevs' ),
+                    'desc' => __( 'Multi checkbox description', 'wedevs' ),
+                    'type' => 'multicheck',
+                    'options' => array(
+                        'one' => 'One',
+                        'two' => 'Two',
+                        'three' => 'Three',
+                        'four' => 'Four'
+                    )
+                ),
+                array(
+                    'name' => 'selectbox',
+                    'label' => __( 'A Dropdown', 'wedevs' ),
+                    'desc' => __( 'Dropdown description', 'wedevs' ),
+                    'type' => 'select',
+                    'default' => 'no',
+                    'options' => array(
+                        'yes' => 'Yes',
+                        'no' => 'No'
+                    )
+                ),
+                array(
+                    'name' => 'password',
+                    'label' => __( 'Password', 'wedevs' ),
+                    'desc' => __( 'Password description', 'wedevs' ),
+                    'type' => 'password',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'file',
+                    'label' => __( 'File', 'wedevs' ),
+                    'desc' => __( 'File description', 'wedevs' ),
+                    'type' => 'file',
+                    'default' => ''
+                )
+            ),
+            'wedevs_advanced' => array(
+                array(
+                    'name' => 'color',
+                    'label' => __( 'Color', 'wedevs' ),
+                    'desc' => __( 'Color description', 'wedevs' ),
+                    'type' => 'color',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'password',
+                    'label' => __( 'Password', 'wedevs' ),
+                    'desc' => __( 'Password description', 'wedevs' ),
+                    'type' => 'password',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'wysiwyg',
+                    'label' => __( 'Advanced Editor', 'wedevs' ),
+                    'desc' => __( 'WP_Editor description', 'wedevs' ),
+                    'type' => 'wysiwyg',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'multicheck',
+                    'label' => __( 'Multile checkbox', 'wedevs' ),
+                    'desc' => __( 'Multi checkbox description', 'wedevs' ),
+                    'type' => 'multicheck',
+                    'default' => array('one' => 'one', 'four' => 'four'),
+                    'options' => array(
+                        'one' => 'One',
+                        'two' => 'Two',
+                        'three' => 'Three',
+                        'four' => 'Four'
+                    )
+                ),
+                array(
+                    'name' => 'selectbox',
+                    'label' => __( 'A Dropdown', 'wedevs' ),
+                    'desc' => __( 'Dropdown description', 'wedevs' ),
+                    'type' => 'select',
+                    'options' => array(
+                        'yes' => 'Yes',
+                        'no' => 'No'
+                    )
+                ),
+                array(
+                    'name' => 'password',
+                    'label' => __( 'Password', 'wedevs' ),
+                    'desc' => __( 'Password description', 'wedevs' ),
+                    'type' => 'password',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'file',
+                    'label' => __( 'File', 'wedevs' ),
+                    'desc' => __( 'File description', 'wedevs' ),
+                    'type' => 'file',
+                    'default' => ''
+                )
+            ),
+            'wedevs_others' => array(
+                array(
+                    'name' => 'text',
+                    'label' => __( 'Text Input', 'wedevs' ),
+                    'desc' => __( 'Text input description', 'wedevs' ),
+                    'type' => 'text',
+                    'default' => 'Title'
+                ),
+                array(
+                    'name' => 'textarea',
+                    'label' => __( 'Textarea Input', 'wedevs' ),
+                    'desc' => __( 'Textarea description', 'wedevs' ),
+                    'type' => 'textarea'
+                ),
+                array(
+                    'name' => 'checkbox',
+                    'label' => __( 'Checkbox', 'wedevs' ),
+                    'desc' => __( 'Checkbox Label', 'wedevs' ),
+                    'type' => 'checkbox'
+                ),
+                array(
+                    'name' => 'radio',
+                    'label' => __( 'Radio Button', 'wedevs' ),
+                    'desc' => __( 'A radio button', 'wedevs' ),
+                    'type' => 'radio',
+                    'options' => array(
+                        'yes' => 'Yes',
+                        'no' => 'No'
+                    )
+                ),
+                array(
+                    'name' => 'multicheck',
+                    'label' => __( 'Multile checkbox', 'wedevs' ),
+                    'desc' => __( 'Multi checkbox description', 'wedevs' ),
+                    'type' => 'multicheck',
+                    'options' => array(
+                        'one' => 'One',
+                        'two' => 'Two',
+                        'three' => 'Three',
+                        'four' => 'Four'
+                    )
+                ),
+                array(
+                    'name' => 'selectbox',
+                    'label' => __( 'A Dropdown', 'wedevs' ),
+                    'desc' => __( 'Dropdown description', 'wedevs' ),
+                    'type' => 'select',
+                    'options' => array(
+                        'yes' => 'Yes',
+                        'no' => 'No'
+                    )
+                ),
+                array(
+                    'name' => 'password',
+                    'label' => __( 'Password', 'wedevs' ),
+                    'desc' => __( 'Password description', 'wedevs' ),
+                    'type' => 'password',
+                    'default' => ''
+                ),
+                array(
+                    'name' => 'file',
+                    'label' => __( 'File', 'wedevs' ),
+                    'desc' => __( 'File description', 'wedevs' ),
+                    'type' => 'file',
+                    'default' => ''
+                )
+            )*/
+        );
+
+        return $settings_fields;
+    }
+
+    function plugin_page() {
+        echo '<div class="wrap">';
+
+        $this->settings_api->show_navigation();
+        $this->settings_api->show_forms();
+
+        echo '</div>';
+    }
+
+    /**
+     * Get all the pages
+     *
+     * @return array page names with key value pairs
+     */
+    function get_pages() {
+        $pages = get_pages();
+        $pages_options = array();
+        if ( $pages ) {
+            foreach ($pages as $page) {
+                $pages_options[$page->ID] = $page->post_title;
+            }
+        }
+
+        return $pages_options;
+    }
+
+}
+endif;
+
+$settings = new WeDevs_Settings_API_Test();
---
+++ inc/clean_up_ui.php
@@ -0,0 +1,138 @@
+<?php
+
+function fontawesome_dashboard() {
+   wp_enqueue_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', '', '4.1.0', 'all'); 
+}
+ 
+add_action('admin_init', 'fontawesome_dashboard');
+
+
+/* Make the Admin UI Cleaner */
+// THIS TAKES AWAY DEFAULT ADMIN FOOTER TEXT AND USES MY OWN
+function replace_footer_version() {
+	echo "You're currently using:<strong> Wordpress ".get_bloginfo('version')."</strong>";
+}
+add_filter( 'update_footer', 'replace_footer_version', '1234');
+
+function hide_help() {
+    echo '<style type="text/css">
+            #contextual-help-link-wrap { display: none !important; }
+            i.fa {text-align:center;width:45px;vertical-align:middle;}
+          </style>';
+}
+add_action('admin_head', 'hide_help');
+
+function fb_admin_footer () {
+	echo '<a href="http://www.fredbradley.co.uk/"><span class="websiteby">a Fred Bradley website</span></a>';
+} 
+add_filter('admin_footer_text', 'fb_admin_footer');
+
+function remove_editor_menu() {
+	remove_action('admin_menu', '_add_themes_utility_last', 101);
+}
+add_action('_admin_menu', 'remove_editor_menu', 1);
+
+function replace_howdy( $wp_admin_bar ) {
+    $my_account=$wp_admin_bar->get_node('my-account');
+    $newtitle = str_replace( 'Howdy,', 'Logged in:', $my_account->title );            
+    $wp_admin_bar->add_node( array(
+        'id' => 'my-account',
+        'title' => $newtitle,
+    ) );
+}
+add_filter( 'admin_bar_menu', 'replace_howdy',25 );
+
+function wps_admin_bar() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('wp-logo');
+    $wp_admin_bar->remove_menu('about');
+    $wp_admin_bar->remove_menu('wporg');
+    $wp_admin_bar->remove_menu('documentation');
+    $wp_admin_bar->remove_menu('support-forums');
+    $wp_admin_bar->remove_menu('feedback');
+    
+}
+add_action( 'wp_before_admin_bar_render', 'wps_admin_bar' );
+
+function my_custom_login_logo() {
+	if (@file_get_contents(get_bloginfo('template_url').'/images/login_page_logo.png')) {
+		$login_logo = get_bloginfo('template_url').'/images/login_page_logo.png';
+	} else {
+		$login_logo = plugins_url('logo-wordpress-login.png', __DIR__);
+	}
+
+    echo '<style type="text/css">
+        h1 a {background-size:auto !important; width:320px !important;height:85px !important;background-image:url('.$login_logo.') !important; }
+    </style>';
+}
+add_action('login_head', 'my_custom_login_logo');
+
+
+
+
+// First, create a function that includes the path to your favicon
+function add_favicon() {
+	if (@file_get_contents(get_bloginfo('template_url').'/images/icons/favicon.png')) {
+		$favicon_url = get_bloginfo('template_url').'/images/icons/favicon.png';
+	} else {
+		$favicon_url = plugins_url('images/websiteby.png', __DIR__);
+	}
+	echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
+}
+  
+// Now, just make sure that function runs when you're on the login page and admin pages  
+add_action('login_head', 'add_favicon');
+add_action('admin_head', 'add_favicon');
+
+
+
+function custom_post_css() {
+   echo "<style type='text/css' media='screen'>
+   	
+       #adminmenu .menu-icon-post div.wp-menu-image:before {
+            font-family:  FontAwesome !important;
+            content: '\\f044'; // this is where you enter the fontawesome font code
+        }
+     </style>";
+}
+add_action('admin_head', 'custom_post_css');
+
+
+
+function load_fb_admin_style(){
+	echo '<style>span.websiteby{background: url("'.plugins_url("websiteby.png", __DIR__).'") no-repeat left center transparent;padding-left: 20px;height:20px;display:block;}</style>';
+}
+add_action('admin_head', 'load_fb_admin_style');
+
+// This just echoes the chosen line, we'll position it later
+function fb_admin_notice() {
+//	echo "<p class='fb_admin_notice' id='bug_report'><a href='http://support.fredbradley.co.uk/open.php' target='_blank'><span class='websiteby'>Bug Report</span></a></p>";
+	echo "<p class='fb_admin_notice' id='issue_tracker'><a href='https://bitbucket.org/fredbradley/student-radio-v3/issues' target='_blank'><i class='fa fa-2x fa-bitbucket'></i>Issue Tracker</a></p>";
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'admin_notices', 'fb_admin_notice' );
+
+// We need some CSS to position the paragraph
+function fb_admin_notice_css() {
+	// This makes sure that the positioning is also good for right-to-left languages
+	$x = is_rtl() ? 'left' : 'right';
+
+	echo "
+	<style type='text/css'>
+	.fb_admin_notice {
+		float: $x;
+		padding-$x: 15px;
+		padding-top: 5px;		
+		margin: 0;
+	}
+	.fb_admin_notice i.fa {
+		color: #205081;
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'fb_admin_notice_css' );
+
+
